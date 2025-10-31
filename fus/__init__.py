23ead2bf9cf463edd975dd53ee7ef1d4d1cc86c0
yanuino <36410910+yanuino@users.noreply.github.com>
2025-10-31 12:09:17 +0100

    Refine error imports in __init__.py

--- fus/__init__.py
+++ fus/__init__.py
@@ -6,4 +6,4 @@ from .firmware import get_latest_version, normalize_vercode, read_firmware_info
 from .decrypt import decrypt_file, get_v2_key, get_v4_key
 from .deviceid import validate_serial, validate_imei, is_device_id_required
 from .responses import parse_inform, InformInfo, get_info_from_inform
-from .errors import *
+from .errors import FUSError, AuthError, InformError, DownloadError, DecryptError, DeviceIdError
